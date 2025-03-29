391b313bace28e9220e18f6b41730471b9eb5c6c
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2025-03-29 08:53:34 +0530

    fixed dashboard redirection on authentication

--- app/page.js
+++ app/page.js
@@ -1,13 +1,24 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
+import { useUser, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { user, isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
-   <div>
-      <Header/>
-      <Hero/>
-   </div>
+    <div>
+      <Header />
+      <Hero />
+    </div>
   );
 }
